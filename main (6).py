1e8c80d89033aebd02679452ffa4df0b2db60638
abty123 <144332772+abty123@users.noreply.github.com>
2023-09-07 15:29:46 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,11 @@
+def Fact(x):
+  if x==1:
+    return 1
+  else:
+    print(x)
+    sum=x*Fact(x-1)
+    return(sum)
+num=int(input("enter a number:"))
+if num>=-1:
+  print("Factorial value",Fact(num))
+  
